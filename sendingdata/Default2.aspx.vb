d17a2ba023583af1394ada9065ae43ff6fc0b725
Anticoder03 <116935348+Anticoder03@users.noreply.github.com>
2024-08-01 06:55:25 +0530

    Add files via upload

---
+++ sendingdata/Default2.aspx.vb
@@ -0,0 +1,10 @@
+﻿
+Partial Class Default2
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Label1.Text = "Your Name is: " + Request.QueryString("Name")
+        Label2.Text = "Your Password is: " + Request.QueryString("Password")
+
+    End Sub
+End Class
